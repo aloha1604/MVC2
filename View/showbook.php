68912d4a6723480d75e69bc23153d4d501bcb440
Ansama <tranminh534@gmail.com>
2020-04-22 10:39:41 +0700

    hoangan update view

---
+++ View/showbook.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quan Li Sach</title>
+	<link href="<?php echo BASE_URL ?>/assets/admin_style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<table>
+		
+	</table>
+</body>
+</html>
